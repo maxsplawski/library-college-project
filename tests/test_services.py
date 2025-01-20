400f33f8e4dc7f58767c5f2af03a9d4f8913a6f9
Max Spławski <mniamek9@gmail.com>
2025-01-20 20:54:54 +0100

    feat: develop test_services

--- tests/test_services.py
+++ tests/test_services.py
@@ -24,3 +24,25 @@ class TestServices(unittest.TestCase):
         self.assertTrue(result)
         self.mock_auth_service.login.assert_called_once_with("user@example.com", "password")
         mock_print.assert_any_call("Logged in")
+
+    @patch("builtins.input", side_effect=["user@example.com", "password"])
+    @patch("getpass.getpass", return_value="password")
+    @patch("builtins.print")
+    def test_user_is_shown_error_message_on_login_failure(self,  mock_print: MagicMock, mock_getpass: MagicMock, mock_input: MagicMock):
+        # given
+        self.mock_auth_service.login.return_value = False
+
+        # when
+        result = self.cli.show_login()
+
+        # then
+        self.assertFalse(result)
+        self.mock_auth_service.login.assert_called_once_with("user@example.com", "password")
+        mock_print.assert_any_call("Invalid credentials, please try again")
+
+    @patch("builtins.input", side_effect=["user@example.com", "password", "password"])
+    @patch("getpass.getpass", return_value="password")
+    @patch("builtins.print")
+    def test_user_can_sign_up(self,  mock_print: MagicMock, mock_getpass: MagicMock, mock_input: MagicMock):
+        # given
+        self.mock_auth_service.get_user.return_value = None
