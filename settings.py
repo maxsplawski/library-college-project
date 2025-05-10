8c858e949bc9f170347aec4ab37f5e704f28059f
Max Spławski <mniamek9@gmail.com>
2025-05-10 16:50:16 +0200

    refactor: refactor settings.py, remove test.py

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 import os
 
+APP_NAME = "Library"
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_DIR = f"{ROOT_DIR}/db"
-APP_NAME = "Library"
 SQLITE_FILENAME = f"{DB_DIR}/library.db"
 INSERT_TESTDATA = False
-EXPORT_DESTINATION = "exports"
+EXPORT_DESTINATION = f"{ROOT_DIR}/exports"
